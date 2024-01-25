43b947ca163b0a417bf1a3e15b54bb1f87712a20
InvaCeniii <57628673+InvaCeniii@users.noreply.github.com>
2024-01-25 16:49:52 +0100

    Add files via upload

---
+++ part01-Part01_07.MessageThreeTimes/src/test/java/MessageThreeTimesTest.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message: ");
+
+
+        String userMessage = scanner.nextLine();
+
+
+        System.out.println(userMessage);
+        System.out.println(userMessage);
+        System.out.println(userMessage);
+
+    }
+}
