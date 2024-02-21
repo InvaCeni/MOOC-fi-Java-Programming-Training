77caf712726eaf121b543da62605c15183872ded
Inva Ceni <57628673+InvaCeni@users.noreply.github.com>
2024-02-21 12:19:47 +0100

    Add files via upload

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/Account.java
@@ -0,0 +1,37 @@
+/* 
+ * DO NOT TOUCH THIS !
+ */
+
+public class Account {
+
+    private double balance;
+    private String owner;
+
+    public Account(String owner, double balance) {
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public void deposit(double amount) {
+        this.balance = this.balance + amount;
+    }
+
+    public void withdrawal(double amount) {
+        this.balance = this.balance - amount;
+    }
+
+    public double saldo() {
+        return this.balance;
+    }
+
+    @Override
+    public String toString() {
+        return this.owner + " balance: " + this.balance;
+    }
+
+    // Method to get the current balance of the account
+    public double getBalance() {
+        return balance;
+    }
+
+}
