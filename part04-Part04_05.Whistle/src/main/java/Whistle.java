99418958256eda457cefeb705da9ebbcb1dd345a
Inva Ceni <57628673+InvaCeni@users.noreply.github.com>
2024-02-26 12:09:23 +0100

    Add files via upload

---
+++ part04-Part04_05.Whistle/src/main/java/Whistle.java
@@ -0,0 +1,12 @@
+public class Whistle {
+    private String sound;
+
+    public Whistle(String whistleSound){
+        this.sound = whistleSound;
+    }
+
+    // Method to print the whistle's sound
+    public void sound() {
+        System.out.println(sound);
+    }
+}
