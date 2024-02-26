465e0d785cd5b32bc7aaabdc9112150079f3db0a
Inva Ceni <57628673+InvaCeni@users.noreply.github.com>
2024-02-26 12:14:56 +0100

    Add files via upload

---
+++ part04-Part04_06.Door/src/main/java/Door.java
@@ -0,0 +1,12 @@
+public class Door {
+
+    //Constructor
+    public Door (){
+    // No variables to initialize in this case
+    }
+
+    public void knock(){
+        System.out.println("Who's there?");
+    }
+
+}
