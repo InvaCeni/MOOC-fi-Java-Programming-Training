6db10b0a0ae6bf20e1284c5277f7cabfa6e57814
Inva Ceni <57628673+InvaCeni@users.noreply.github.com>
2024-02-26 11:53:32 +0100

    Add files via upload

---
+++ part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,12 @@
+public class Room {
+    private String code;
+    private int seats;
+
+
+    // Constructor
+    public Room(String code, int seats) {
+        this.code = code;
+        this.seats = seats;
+    }
+
+}
