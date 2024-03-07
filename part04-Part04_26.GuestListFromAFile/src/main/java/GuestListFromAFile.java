43c0e81533aee333d1a88ff104dce88febfc2569
Inva Ceni <57628673+InvaCeni@users.noreply.github.com>
2024-03-07 09:32:22 +0100

    Add files via upload

---
+++ part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -0,0 +1,44 @@
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.io.IOException;
+
+public class GuestListFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        ArrayList<String> list = new ArrayList<>();
+// Read names from the file and populate the list
+        try {
+            Files.lines(Paths.get(file))
+                    .forEach(list::add);
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+            return;
+        }
+
+        System.out.println("");
+        System.out.println("Enter names, an empty line quits.");
+
+        while (true) {
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            if (list.contains(name)) {
+                System.out.println("The name is on the list.");
+            } else {
+                System.out.println("The name is not on the list.");
+            }
+        }
+
+        System.out.println("Thank you!");
+    }
+}
