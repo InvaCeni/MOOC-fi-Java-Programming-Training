2faa2c53441c9a32ff97dbcb3ec9327e66ecca97
Inva Ceni <57628673+InvaCeni@users.noreply.github.com>
2024-03-25 16:06:36 +0100

    Add files via upload

---
+++ part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
@@ -0,0 +1,10 @@
+
+import java.util.ArrayList;
+
+public class NullPointerExceptionProgram {
+
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
