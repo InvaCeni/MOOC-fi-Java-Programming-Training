aa0ac7c06a566d06490fee0ab6b5388fbce15bdb
Inva Ceni <57628673+InvaCeni@users.noreply.github.com>
2024-04-03 12:53:25 +0200

    Add files via upload

---
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -0,0 +1,60 @@
+
+public class Money {
+
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros() {
+        return euros;
+    }
+
+    public int cents() {
+        return cents;
+    }
+
+    public String toString() {
+        String centString = (cents < 10) ? "0" + cents : String.valueOf(cents);
+        return euros + "." + centString + "e";
+    }
+
+    public Money plus(Money addition) {
+        int newEuros = this.euros + addition.euros;
+        int newCents = this.cents + addition.cents;
+
+        if (newCents >= 100) {
+            newEuros++;
+            newCents -= 100;
+        }
+
+        return new Money(newEuros, newCents);
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros < compared.euros) {
+            return true;
+        } else if (this.euros == compared.euros && this.cents < compared.cents) {
+            return true;
+        }
+        return false;
+    }
+
+    public Money minus(Money decreaser) {
+        int newEuros = this.euros - decreaser.euros;
+        int newCents = this.cents - decreaser.cents;
+
+        if (newEuros < 0 || (newEuros == 0 && newCents < 0)) {
+            newEuros = 0;
+            newCents = 0;
+        } else if (newCents < 0) {
+            newEuros--;
+            newCents += 100;
+        }
+
+        return new Money(newEuros, newCents);
+    }
+}
